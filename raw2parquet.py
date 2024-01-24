c7c685a55df6e701b0c89fdf0d3efb844d9a1d7a
tobiasbrammer <70036361+tobiasbrammer@users.noreply.github.com>
2024-01-24 15:23:41 +0100

    Update raw2parquet.py

--- raw2parquet.py
+++ raw2parquet.py
@@ -14,31 +14,37 @@ pattern = re.compile(r'_(.*?)-')
 
 # For files that begins with prices
 prices = [f for f in files if f.startswith('prices')]
-prices.sort()
 
 # Create empty dataframe
 df_list = []
 # For each file, add it to df
 for file in prices:
     # Read csv file and try to parse dates
-    df_temp = pl.read_csv('data_raw/' + file)
+    df_temp = pl.scan_csv('data_raw/' + file, low_memory=True)
+    # Force StockOpen  ┆ StockClose ┆ StockHigh  ┆ StockLow   ┆ StockVol to f64
+    df_temp = df_temp.with_columns(
+        pl.col('StockOpen').cast(pl.Float64),
+        pl.col('StockClose').cast(pl.Float64),
+        pl.col('StockHigh').cast(pl.Float64),
+        pl.col('StockLow').cast(pl.Float64),
+        pl.col('StockVol').cast(pl.Float64)
+    )
     # If df_temp is empty, continue
-    if df_temp.shape[0] == 0:
-        continue
-    else:
-        # Extraxt the ticker from the filename and add it as a column
-        ticker = re.search(pattern, file).group(1)
-        # Add ticker as a column
-        df_temp = df_temp.with_columns(pl.lit(ticker).alias("ticker"))
-        # Add df_temp to df_list
-        df_list.append(df_temp)
+    # Extraxt the ticker from the filename and add it as a column
+    ticker = re.search(pattern, file).group(1)
+    # Add ticker as a column
+    df_temp = df_temp.with_columns(pl.lit(ticker).alias("ticker"))
+    # Add df_temp to df_list
+    df_list.append(df_temp)
+    del df_temp
     print(ticker)
 
 
 print('Concatenating dataframes...')
+
 df = pl.concat(df_list)
 
-df = (
+(
     df
     .with_columns(
         pl.col('StartTime').str.strptime(pl.Datetime, format='%Y-%m-%d %H:%M:%S').cast(pl.Datetime).alias('datetime'),
@@ -48,11 +54,11 @@ df = (
     .drop('StartTime')
 )
 
-# Save df as parquet
-df.write_parquet('prices.parquet')
+df.sink_parquet('prices.parquet')
+
 
 # Delete all from workspace expect globals
-del df_list, file, ticker, df_temp, df, prices
+del df_list, file, ticker, df_temp, prices
 
 print('Getting daily prices...')
 
@@ -65,21 +71,11 @@ lf_dvol = lf.group_by(['date', 'ticker']).agg(pl.sum('StockVol').alias('daily_vo
 # Join lf and lf_dvol
 lf = lf.join(lf_dvol, on=['date', 'ticker'])
 
-df = lf.collect(streaming=True)
-
 # Only keep rows with time 20:59:00
-lf.filter(pl.col('time').str.contains('20:59:00')).collect().write_parquet('prices_daily.parquet')
-
-# Get daily volume by grouping by ticker and date and summing StockVol
-
-# Group by ticker and date and sum StockVol
-lf = lf.groupby(['ticker', 'date']).agg(pl.sum('StockVol').alias('daily_volume'))
-
-lf.filter(pl.col('StartTime').str.contains('20:59:00')).collect().write_parquet('vol_daily.parquet')
-
+lf.filter(pl.col('time').str.contains('20:59:00')).sink_parquet('prices_daily.parquet')
 
 # Clear LazyFrame
-del lf
+del lf, lf_dvol
 
 ##################### COACS #####################
 
@@ -87,7 +83,6 @@ print('Getting CoACS...')
 
 # For files that start with coacs
 coacs = [f for f in files if f.startswith('coacs')]
-coacs.sort()
 
 # Create empty dataframe
 df_list = []
@@ -95,23 +90,18 @@ df_list = []
 # For each file, add it to df
 for file in coacs:
     # Read csv file and try to parse dates
-    df_temp = pl.read_csv('data_raw/' + file)
+    df_temp = pl.scan_csv('data_raw/' + file)
     # If df_temp is empty, continue
-    if df_temp.shape[0] == 0:
-        continue
-    else:
-        ticker = re.search(pattern, file).group(1)
-        # Add ticker as a column
-        df_temp = df_temp.with_columns(pl.lit(ticker).alias("ticker"))
-        df_list.append(df_temp)
+    ticker = re.search(pattern, file).group(1)
+    # Add ticker as a column
+    df_temp = df_temp.with_columns(pl.lit(ticker).alias("ticker"))
+    df_list.append(df_temp)
     print(ticker)
 
-df = pl.concat(df_list)
-
-# Save df as parquet
-df.write_parquet('coacs.parquet')
+pl.concat(df_list).sink_parquet('coacs.parquet')
 
 # Delete variables from workspace
-del df_list, file, ticker, df_temp, df, coacs, files, pattern
-
+del df_list, file, ticker, df_temp, coacs, files, pattern
+print('')
+print('')
 print('Done!')
