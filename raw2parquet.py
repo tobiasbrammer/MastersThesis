85401b0d455c17132d23e6cdd7c2f7533c3d9aa5
Tobias Brammer <tobias.brammer@gmail.com>
2024-02-10 14:36:22 +0100

    Fix data import

--- raw2parquet.py
+++ raw2parquet.py
@@ -10,29 +10,35 @@ start = time()
 # %% ################## CSV TO PARQUET ##################
 
 # # Get all files in data_raw_csv
-# files = [f for f in os.listdir('data_raw_csv') if f.endswith('.csv')]
+files = [f for f in os.listdir('data_raw_csv') if f.endswith('.csv')]
 
 # # Convert each file to parquet
-# print('Converting CSV files to Parquet...')
-# for file in files:
-#     # Construct the file path
-#     file_path = os.path.join('data_raw_csv', file)
-#     # Read CSV file and try to parse dates
-#     df = pl.scan_csv(file_path)
-#     # Write to Parquet
-#     df.sink_parquet(file_path.replace('.csv', '.parquet'))
-#     del df, file, file_path
+print('Converting CSV files to Parquet...')
+for file in files:
+    # Construct the file path
+    file_path = os.path.join('data_raw_csv', file)
+    # If file path contains Visa, skip it
+    #if 'Visa' in file_path:
+    #    continue
+    #if 'Interpublic_Group' in file_path:
+    #    continue
+    # Read CSV file and try to parse dates
+    df = pl.scan_csv(file_path)
+    # Write to Parquet
+    print(f'Converted {file_path.replace("csv", "parquet")}')
+    df.sink_parquet(file_path.replace('csv', 'parquet'))
+    del df, file, file_path
 
-files = [f for f in os.listdir("data_raw_parquet") if f.endswith(".parquet")]
-
-# Define the pattern using regular expression
-pattern = re.compile(r"_(.*?)-")
 
 
 # %% ################## PRICES ##################
-
+files = [f for f in os.listdir("data_raw_parquet") if f.endswith(".parquet")]
+pattern = re.compile(r"_(.*?)-")
 # For files that begin with prices
 prices = sorted([f for f in files if f.startswith("prices")])
+
+# Omit ROL from prices
+
 
 # Create an empty list to store DataFrames
 df_list = []
@@ -47,12 +53,33 @@ for file in prices:
     # Read CSV file and try to parse dates
     df_temp = pl.scan_parquet(file_path).with_columns(
         **{col: pl.col(col).cast(pl.Float64) for col in numeric_cols}
-    )
+    ).select([
+        "StartTime",
+        "StockClose",
+        "StockVol",
+    ])
     # Extract the ticker from the filename and add it as a column
     ticker = re.search(pattern, file).group(1)
+    # Strip ".I" from the end of the ticker
+    ticker = ticker.rstrip(".I")  # new line
+    # If the ticker is ROL, skip it
+    #if ticker == "ROL":
+    #    print(f"Skipping {ticker}")
+    #    continue
     df_temp = df_temp.with_columns(pl.lit(ticker).alias("ticker"))
+    # Test if df_temp is corrupted, if so skip it
+    if df_temp.fetch().shape[0] == 0:
+        print(f"Failed to read {ticker}")
+        continue
+    # Test if collect fails on df_temp, if so skip it
+    try:
+        df_temp.collect()
+    except:
+        print(f"Failed to collect {ticker}")
+        continue
     # Add df_temp to df_list
     df_list.append(df_temp)
+
     del df_temp, file, file_path, ticker
 
 print("Concatenating dataframes...")
