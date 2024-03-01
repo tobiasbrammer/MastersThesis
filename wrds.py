c0df9986406106ed279aa9757a4b73b928ada125
tobiasbrammer <70036361+tobiasbrammer@users.noreply.github.com>
2024-03-01 08:21:55 +0100

    wrds

--- wrds.py
+++ wrds.py
@@ -1,56 +1,57 @@
 import pandas as pd
-import os
 import wrds
+from tqdm import tqdm
+import polars as pl
 
 db = wrds.Connection(wrds_username='tobiasbrammer')
 
 # Set your start and end dates
-start_date = "2019-01-01"
-end_date = "2023-12-31"
+start_date = "2022-12-31"
+end_date = "2024-01-01"
 
 # List fundamental variables
-fund_vars = db.describe_table(library="comp", table="funda")
+# fund_vars = db.describe_table(library="comp", table="funda")
 
 # Define the variables of interest
 variables = [
-    "tic", # Ticker
-    "cusip", # CUSIP
-    "gvkey", # Global company key
-    "datadate", # Date
-    "at", # Total assets
-    "lt", # Total liabilities
-    "prc", # Price
-    "act", # Current assets
-    "che", # Cash and equivalents
-    "lct", # Current liabilities
-    "dlc", # Current debt
-    "txp", # Income taxes payable
-    "sale", # Sales
-    "txditc", # Deferred income taxes
-    "seq", # Shareholders' equity
-    "ceq", # Common equity
-    "pstkl", # Preferred stock
-    "pstk", # Preferred stock
-    "ni", # Net income
-    "dp", # Dividends paid
-    "wcapch", # Working capital change
-    "capx", # Capital expenditures
-    "txdb", # Debt
+    "tic",  # Ticker
+    "cusip",  # CUSIP
+    "gvkey",  # Global company key
+    "datadate",  # Date
+    "at",  # Total assets
+    "lt",  # Total liabilities
+    "act",  # Current assets
+    "che",  # Cash and equivalents
+    "lct",  # Current liabilities
+    "dlc",  # Current debt
+    "txp",  # Income taxes payable
+    "sale",  # Sales
+    "txditc",  # Deferred income taxes
+    "seq",  # Shareholders' equity
+    "ceq",  # Common equity
+    "pstkl",  # Preferred stock
+    "pstk",  # Preferred stock
+    "ni",  # Net income
+    "dp",  # Dividends paid
+    "capx",  # Capital expenditures
+    "txdb",  # Debt
 ]
 
-# List variables from 'variables' that are not in 'fund_vars'
-missing_vars = [var for var in variables if var not in fund_vars["name"].values]
+tickers = pd.read_parquet('daily.parquet')['ticker'].unique()
+tickers = tickers[0:10]
 
-# Remove missing variables from 'variables'
-variables = [var for var in variables if var not in missing_vars]
+# Join tickers in a string like 'AAPL', 'MSFT', ...
+tickers = [f"'{t}'" for t in tickers]
 
-# Download Compustat data
+# Create the compustat query
 compustat_query = f"""
     SELECT {', '.join(variables)}
     FROM comp.funda
     WHERE indfmt = 'INDL' AND datafmt = 'STD' AND consol = 'C' AND datadate BETWEEN '{start_date}' AND '{end_date}'
 """
-compustat_data = db.raw_sql(compustat_query)
 
-# Close the connection
-db.close()
+df = db.raw_sql(compustat_query)
+
+df[~(df['tic'].isna())].pivot(columns='tic').pl.sink_parquet('compustat.parquet')
+
+
