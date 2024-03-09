7e756beb920077a0d8deac6dff2d097bb2ffcd6c
LucasBraae <braae9@hotmail.com>
2024-03-09 17:22:21 +0100

    updates

--- run_models.py
+++ run_models.py
@@ -1,6 +1,7 @@
 # Import packages and functions
 from functions import *
 import pickle
+import yaml
 
 ########################################################################################################################
 # ToDo's:
@@ -15,6 +16,7 @@ import pickle
 #       - Done (using yf's 3 months treasure bill)
 
 # 2. Set everything up for the other models
+#   bonus) Set up config for all models (Start with FFT)
 #   a) Set up for OU model (Everything is ready to be fit into the train/test functions I believe)
 
 #   b) Set up for CNN model
@@ -36,90 +38,73 @@ import pickle
 ########################################################################################################################
 # Initialize parameters
 ########################################################################################################################
-# Get data - (Temporary - maybe keep for the daily_dates?)
-# Test if daily_data.parquet exists, else get it
-if not os.path.exists('daily_data.parquet'):
-    print("Downloading daily data")
-    get_daily_data()
+# Load data
+df = np.load('daily_data_run.npy')
+daily_dates = np.load('daily_dates.npy', allow_pickle=True)
 
-df = pd.read_parquet('daily_data.parquet')[['return']][1:]
-df.replace(np.nan, 0, inplace=True)
-daily_dates = df.index.date
-df = np.array(df)
-df = np.nan_to_num(df, nan=0, posinf=0, neginf=0)
-
-res_pca_path = "factor_outputs/OOSResiduals_PCA_factor5_rollingwindow_60.npy"
-# residual_weights = None  # The residual composition matrix
-use_residual_weights = True  # Should be True
-
-# FFT model_tag
-model_tag = "FFT"  # ToDo: What do we want to include? lookback, objective, trans_cost, hold_cost, num_epochs, lr ??
-
-# Output path
+# Set output path and cwd
 outdir = os.path.join(os.getcwd(), "Outputs")
+cwd = os.getcwd()
 
 # Initialize torch
 torch.set_default_dtype(torch.float)
 torch.autograd.set_detect_anomaly(True)
 torch.backends.cudnn.deterministic = False
 
-# Model parameters
+# Choose model
+with open('configs/FFT.yaml', 'r') as file:
+    config = yaml.safe_load(file)
 model_name = FFT_FFN
-model_name_ = "FFT_FFN"  # For saving in correct directory
-factor_models = {"IPCA": [5], "PCA": [5], "FamaFrench": [5]}  # We are only running 5-factor models (for now)
-objective = 'sharpe'  # Can be 'sharpe', 'meanvar' or 'sqrtMeanSharpe'
-cwd = os.getcwd()
+preprocess = preprocess_fourier
+
+# Load factors - ToDo: Files for our residual data should be in the list:
+factors = [config["res_pca_path"]]  # ['PCA', 'IPCA', 'FamaFrench']
+
 results_dict = {}
-
-# Set up data
-# Load IPCA, PCA, and Fama-French factors - ToDo: Files for our residual data should be in the list:
-factors = [res_pca_path]  # ['PCA', 'IPCA', 'FamaFrench']
-
 for i in range(len(factors)):
 
     print(f"Testing factor model: {factors[i]}")
     start_time = time.time()
 
     residuals = np.load(factors[i])
-    # Residuals er TxNxN fordi at den indeholder vores portfølje for HVER asset
 
-    if use_residual_weights:
-        # ToDo: Skal laves noget så den kan hente de rigtige
-        residual_weights = np.load("factor_outputs/OOSResidualsmatrix_PCA_factor5_rollingwindow_60.npy")
+    if config["use_residual_weights"]:
+        # ToDo: Skal laves noget så den kan hente dem ordenligt
+        residual_weights = np.load(config["residual_weights"])
     else:
         residual_weights = None
 
     # Define model
     model = model_name()
-    preprocess = preprocess_fourier
-    model_tag = model_tag
+    model_tag = config["model_tag"]
 
     print("Starting: " + model_tag)
 
-    outdir = os.path.join(cwd, "Outputs", model_name_)
+    outdir = os.path.join(cwd, "Outputs", config["model_name_"])
     if not os.path.exists(outdir):
         os.makedirs(outdir)
 
     # Estimate (train) model
     # ToDo: So should we use data from 1945-1998 for this training func. and then rest for test?
-    # ToDo: Costs are wrong - They have length_training=1000 and test_size=125 and batch_size=125
     rets_train, sharpe_train, ret_train, std_train, turnover_train, short_proportion_train = estimate(
-        Data=residuals, model=FFT_FFN(), preprocess=preprocess_fourier,
-        residual_weights=residual_weights, save_params=True, force_retrain=True, parallelize=False,
-        log_dev_progress_freq=10,
-        device=None, device_ids=[0, 1, 2, 3, 4, 5, 6, 7], output_path=outdir, num_epochs=10,
-        lr=0.001, early_stopping=False, model_tag=model_tag, batchsize=50, length_training=200, test_size=50,
-        lookback=30, trans_cost=0, hold_cost=0, objective=objective
+        Data=residuals, model=model, preprocess=preprocess_fourier, residual_weights=residual_weights, save_params=True,
+        force_retrain=config["force_retrain"], parallelize=config["parallelize"], log_dev_progress_freq=10, device=None,
+        device_ids=[0, 1, 2, 3, 4, 5, 6, 7], output_path=outdir, num_epochs=config["num_epochs"], lr=config["lr"],
+        early_stopping=config["early_stopping"], model_tag=model_tag, batchsize=config["batchsize"],
+        length_training=config["length_training"], test_size=config["retrain_freq"], lookback=config["lookback"],
+        trans_cost=config["trans_cost"], hold_cost=config["hold_cost"], objective=config["objective"]
     )
 
     # Test model
     rets_test, sharpe_test, ret_test, std_test, turnover_test, short_proportion_test = test(
-        Data=residuals, daily_dates=daily_dates, model=FFT_FFN(),
-        preprocess=preprocess_fourier, residual_weights=residual_weights, save_params=True, force_retrain=True,
-        parallelize=False, log_dev_progress_freq=10, log_plot_freq=149, device=None,
-        device_ids=[0, 1, 2, 3, 4, 5, 6, 7], output_path=outdir,
-        num_epochs=10, lr=0.001, early_stopping=False, model_tag=model_tag, batchsize=50, retrain_freq=125,
-        rolling_retrain=True, length_training=200, lookback=30, trans_cost=0, hold_cost=0, objective=objective
+        Data=residuals, daily_dates=daily_dates, model=model, preprocess=preprocess_fourier,
+        residual_weights=residual_weights, force_retrain=config["force_retrain"], parallelize=config["parallelize"],
+        log_dev_progress_freq=10, log_plot_freq=149, device=None, device_ids=[0, 1, 2, 3, 4, 5, 6, 7],
+        output_path=outdir, num_epochs=config["num_epochs"], lr=config["lr"], early_stopping=config["early_stopping"],
+        model_tag=model_tag, batchsize=config["batchsize"], retrain_freq=config["retrain_freq"],
+        rolling_retrain=config["rolling_retrain"], length_training=config["length_training"],
+        lookback=config["lookback"], trans_cost=config["trans_cost"], hold_cost=config["hold_cost"],
+        objective=config["objective"]
     )
 
     results_dict[model_tag] = {
@@ -135,4 +120,4 @@ for i in range(len(factors)):
     with open(f"{cwd}/results/{model_tag}_results.pkl", "wb") as f:
         pickle.dump(results_dict, f)
 
-    print(f"Time for {model_name_} factor model {factors[i]}: {(time.time() - start_time) / 60} minutes")
+    print(f"Time for {str(model_name)} factor model {factors[i]}: {(time.time() - start_time) / 60} minutes")
