ae974ef314e3b22dcfb07faf52f96317478fc184
tobiasbrammer <70036361+tobiasbrammer@users.noreply.github.com>
2024-03-18 13:12:02 +0100

    working on fama french

--- debug_ipca.py
+++ debug_ipca.py
@@ -1,6 +1,8 @@
+import factor_models
 import factor_models as fm
-import wrds_function
+import wrds_function as wrds
 
-wrds_function.process_compustat(save=True)
+# wrds.process_compustat(save=True)
 
-fm.run_ipca()
+
+factor_models.run_FF(sizeWindow=[60], intitialOOSYear=1998, capProportion=[0.001])
